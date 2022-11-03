579fc9d29dee45c6644a663d3ac90e8ec2323bd5
maiyanglor <maiyang.lor14@gmail.com>
2022-11-02 20:07:49 -0700

    add:feats:length console logs and typeof

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const word = [`s`, `u`, `p`, `e`, `r`, `c`, `a`, `l`, `i`, `f`, `r`, `a`, 
+`g`, `i`, `l`, `i`, `s`, `t`, `i`, `c`, `e`, `x`, `p`, `i`, `a`, `l`, `i`,
+`d`, `o`, `c`, `i`, `o`, `u`, `s`];
+console.log(word.length);
+
+console.log(2);
+
+const pi = BigInt(31415926535897); 
+console.log(pi); 
+
+console.log(typeof 31415926535897);
+
+
